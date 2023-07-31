09c4c7a5848d44fb054947b14c9a656864ee1dcd
Ashalatha A M <140400900+aasha-hpe@users.noreply.github.com>
2023-07-31 13:50:53 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -78,7 +78,7 @@ class TestUninstallComponents(unittest.TestCase):
 
     def test_uninstall_docker_image(self):
 
-        self.mock_UninstallComponents.uninstall_docker_image('image1', '1.2.1', self.mock_UninstallComponents.mock_docker_api)
+        self.mock_UninstallComponents.uninstall_docker_image(self.mock_UninstallComponents,'image1', '1.2.1', self.mock_UninstallComponents.mock_docker_api)
         self.mock_UninstallComponents.mock_docker_api.delete_image.assert_called_once_with('image1', '1.2.1')
 			
     def test_uninstall_docker_image_err(self):
