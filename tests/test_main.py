121e1bb4423cfa552025ce4e8f10e2b5a2b6848a
Ashalatha A M <140400900+aasha-hpe@users.noreply.github.com>
2023-07-31 17:25:48 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -90,7 +90,7 @@ class TestUninstallComponents(unittest.TestCase):
 		
         with self.assertRaises(ProductInstallException):
             self.mock_UninstallComponents.uninstall_docker_image('image1', '2.0.0', self.mock_UninstallComponents.mock_docker_api)
-	    self.mock_UninstallComponents.mock_docker_api.delete_image('image1', '2.0.0')
+            self.mock_UninstallComponents.mock_docker_api.delete_image('image1', '2.0.0')
             self.mock_UninstallComponents.mock_docker_api.delete_image.assert_called_once_with('image1', '2.0.0', self.mock_UninstallComponents.mock_docker_api)
 		
         self.mock_print.assert_called_once_with(
