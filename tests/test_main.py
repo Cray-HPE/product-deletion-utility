fc11ef6e592012195bcaffbcebc21804c2c88397
Srinivas-Anand-HPE <119280543+Srinivas-Anand-HPE@users.noreply.github.com>
2023-08-01 12:20:08 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -230,6 +230,7 @@ class TestDeleteProductComponent(unittest.TestCase):
     def setUp(self):
         """Set up mocks"""
         self.mock_delete_product_component= DeleteProductComponent()
+        self.mock_delete_component._get_k8s_api= Mock()
         self.mock_delete_product_component.get_product= Mock()
         self.mock_delete_product_component.mock_docker_api=Mock()
         self.mock_delete_product_component.mock_nexus_api=Mock()
