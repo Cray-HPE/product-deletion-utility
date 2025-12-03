e7886812ac48ed6654e722fb489cb5e56a67e2c9
Srinivas-Anand-HPE <srinivas-anand-amsuri@hpe.com>
2025-12-03 12:53:41 +0530

    CASMPET-7634 git-core  required modules to fix CVEs

--- zypper.sh
+++ zypper.sh
@@ -12,7 +12,7 @@ zypper --non-interactive ar ${SLES_MIRROR}/Products/SLE-Module-Basesystem/15-SP7
 zypper --non-interactive ar --no-gpgcheck ${CSM_RPMS_HPE_STABLE}/?auth=basic CSM-SLE-15SP7
 zypper update -y
 zypper search --details git-core
-zypper info --all git-core
+zypper info git-core
 zypper search-packages git-core
 zypper install -y craycli git-core bash python311-base curl jq
 zypper clean -a && zypper --non-interactive rr --all && rm -f /etc/zypp/repos.d/*
